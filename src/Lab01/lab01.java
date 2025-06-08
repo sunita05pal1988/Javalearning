1907cce64ce5b6905791fb60f425953b59d313af
sunita05pal1988 <sunita05pal1988@gmail.com>
2025-06-08 20:26:35 +0530

    Initial commit

---
+++ src/Lab01/lab01.java
@@ -0,0 +1,9 @@
+package Lab01;
+
+public class lab01 {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+    }
+
+}
---
+++ src/Lab01/lab02.java
@@ -0,0 +1,7 @@
+package Lab01;
+
+public class lab02 {
+    public static void main(String[] args){
+        System.out.println("Share to Git hub");
+    }
+}
