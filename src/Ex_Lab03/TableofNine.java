512104dec5c33070e0971f0095318b86bdf4fe35
sunita05pal1988 <sunita05pal1988@gmail.com>
2025-06-11 01:29:50 +0530

    New classess

---
+++ src/Ex_Lab03/TableofNine.java
@@ -0,0 +1,12 @@
+package Ex_Lab03;
+
+public class TableofNine {
+    public static void main(String[] args) {
+        int number = 9;
+        for(int i=1;i<=10;i++){
+
+            System.out.printf("%d * %d = %d\n" , number, i,  number * i);
+        }
+
+    }
+}
